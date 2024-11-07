317b5851fb9f380f613f79fca61fd3b641b4300e
Jose Tissera <Jtissera@fi.uba.ar>
2024-11-07 11:50:30 -0300

    Agregamos el home V1 a nuestro template
    ID de tarea: 6727b4d4f8689a1298fe1d45

--- frontend/app.py
+++ frontend/app.py
@@ -2,30 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/index-1')
+
+@app.route("/index-1")
 def index_1():
     return render_template("index-1.html")
 
-@app.route('/index-2')
+
+@app.route("/index-2")
 def index_2():
     return render_template("index-2.html")
 
-@app.route('/index-3')
+
+@app.route("/index-3")
 def index_3():
     return render_template("index-3.html")
 
-@app.route('/index-4')
+
+@app.route("/index-4")
 def index_4():
     return render_template("index-4.html")
 
-@app.route('/index-5')
+
+@app.route("/index-5")
 def index_5():
     return render_template("index-5.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port = 5050)
+@app.route("/homeV1")
+def homeV1():
+    return render_template("homeV1.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
