59048655617c795a5e649ea43a36791ab8de0a08
Francec04 <fcecchi@fi.uba.ar>
2024-11-07 11:11:10 -0300

    672cca298f594b44482655f8
    Creacion de la app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route('/index-1')
+def index_1():
+    return render_template("index-1.html")
+
+@app.route('/index-2')
+def index_2():
+    return render_template("index-2.html")
+
+@app.route('/index-3')
+def index_3():
+    return render_template("index-3.html")
+
+@app.route('/index-4')
+def index_4():
+    return render_template("index-4.html")
+
+@app.route('/index-5')
+def index_5():
+    return render_template("index-5.html")
 
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port = 5050)
