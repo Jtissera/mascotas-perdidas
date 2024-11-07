a5c3cbcf2e832dec782d33fb28c1d7eff936d0c3
Jose Tissera <Jtissera@fi.uba.ar>
2024-11-07 13:12:20 -0300

    Se agrego el nav y se comenzo el desarrolo del template perdi_mi_mascota
    ID de la tarea:6727b4d4f8689a1298fe1d45

--- frontend/app.py
+++ frontend/app.py
@@ -3,37 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
 
-@app.route("/index-1")
-def index_1():
-    return render_template("index-1.html")
+@app.route("/perdi_mi_mascota")
+def perdi_mi_mascota():
+    return render_template("perdi_mi_mascota.html")
 
 
-@app.route("/index-2")
-def index_2():
-    return render_template("index-2.html")
-
-
-@app.route("/index-3")
-def index_3():
-    return render_template("index-3.html")
-
-
-@app.route("/index-4")
-def index_4():
-    return render_template("index-4.html")
-
-
-@app.route("/index-5")
-def index_5():
-    return render_template("index-5.html")
-
-
-@app.route("/homeV1")
+@app.route("/")
 def homeV1():
     return render_template("homeV1.html")
 
