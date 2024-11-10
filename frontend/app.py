c980d63eb2c4e1d10b5dc98e84e58179c322cd49
vouteda <valenfulano@gmail.com>
2024-11-10 18:45:07 -0300

    3

--- frontend/app.py
+++ frontend/app.py
@@ -30,15 +30,26 @@ def faq():
 def info():
     return render_template("info.html")
 
+def obtener_mascota_por_id(mascota_id):
+    # Search for the pet in the in-memory list
+    for mascota in mascotas:
+        if mascota['id'] == mascota_id:
+            return mascota
+    return None  # Return None if no matching pet is found
+
 @app.route('/mascota/<int:mascota_id>')
 def mostrar_mascota(mascota_id):
     mascota = obtener_mascota_por_id(mascota_id)  # Una función que busca la mascota por ID
     return render_template('mascota_perdida.html', mascota=mascota)
 
 mascotas = [
-    {'id': 1, 'nombre': 'Firulais', 'animal': 'Perro', 'raza': 'Labrador', 'lugar_perdido': 'Parque Central', 'fecha_perdida': '2024-11-01', 'info_adicional': 'Llevaba un collar rojo'},
-    {'id': 2, 'nombre': 'Michi', 'animal': 'Gato', 'raza': 'Siames', 'lugar_perdido': 'Calle 42', 'fecha_perdida': '2024-10-29', 'info_adicional': 'Es muy tímido'},
-    # Add more mascotas as needed
+    {'id': 1, 'nombre': '/', 'animal': 'Perro', 'raza': 'Labrador', 'lugar_perdido': 'Parque Central', 'fecha_perdida': '2024-11-01', 'info_adicional': 'Llevaba un collar rojo'},
+    {'id': 2, 'nombre': 'taya', 'animal': 'Gato', 'raza': 'Siames', 'lugar_perdido': 'Calle 42', 'fecha_perdida': '2024-10-29', 'info_adicional': 'Es muy tímido'},
+    {'id': 3, 'nombre': 'maco', 'animal': 'Gato', 'raza': 'Siames', 'lugar_perdido': 'Calle 42', 'fecha_perdida': '2024-10-29', 'info_adicional': 'Es muy tímido'},
+    {'id': 4, 'nombre': 'caco', 'animal': 'Gato', 'raza': 'Siames', 'lugar_perdido': 'Calle 42', 'fecha_perdida': '2024-10-29', 'info_adicional': 'Es muy tímido'},
+    {'id': 5, 'nombre': 'paco', 'animal': 'Gato', 'raza': 'Siames', 'lugar_perdido': 'Calle 42', 'fecha_perdida': '2024-10-29', 'info_adicional': 'Es muy tímido'},
+    {'id': 6, 'nombre': 'naco', 'animal': 'Gato', 'raza': 'Siames', 'lugar_perdido': 'Calle 42', 'fecha_perdida': '2024-10-29', 'info_adicional': 'Es muy tímido'}
+    
 ]
 
 @app.route('/test_mascota')
